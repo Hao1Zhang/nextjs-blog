1e210dfa27f59aff8a3adf30ee2e09f5072d4cf7
Hao1Zhang <87103591+Hao1Zhang@users.noreply.github.com>
2021-08-04 00:16:19 +0930

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>aaaaa</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
